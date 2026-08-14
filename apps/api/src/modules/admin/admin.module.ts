3bb222cb9962fba383583dde2390cd70ed8935cf
shikharshaurya <shikharshaurya.01@gmail.com>
2026-08-14 19:06:13 +0530

    fix: wire applications module into admin

--- apps/api/src/modules/admin/admin.module.ts
+++ apps/api/src/modules/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { AdminFeaturesController } from './admin-features.controller';
 import { FeatureFlagService } from './feature-flag.service';
 import { AdminApplicationsController } from './admin-applications.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ApplicationsModule } from '../applications/applications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ApplicationsModule],
   controllers: [AdminController, AdminFeaturesController, AdminApplicationsController],
   providers: [AdminService, FeatureFlagService],
   exports: [AdminService, FeatureFlagService],
